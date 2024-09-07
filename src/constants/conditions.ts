4497928f292e56ea2af0b513a89a8f280bcb62a6
awyn <alwin.puche16@gmail.com>
2024-09-07 23:17:14 +0800

    add conditions and services constants and images

---
+++ src/constants/conditions.ts
@@ -0,0 +1,56 @@
+import Diastema from '@/assets/images/conditions/condition-1.png'
+import Malocclusion from '@/assets/images/conditions/condition-2.png'
+import ToothAgenesis from '@/assets/images/conditions/condition-3.png'
+import Periodontal from '@/assets/images/conditions/condition-4.png'
+import ToothDecay from '@/assets/images/conditions/condition-5.png'
+import CrackedToothSyndrome from '@/assets/images/conditions/condition-6.png'
+
+export const conditions = [
+    {
+        title: 'Diastema',
+        description: 'refers to a gap between your teeth. Gaps can occur anywhere in your mouth, but they’re most common between your two front teeth. This condition is usually a cosmetic concern, but sometimes, it’s related to gum disease. Diastema treatments include dental bonding, porcelain veneers and braces.',
+        img: Diastema
+    },
+    {
+        title: "Malocclusion ",
+        description: `of the teeth is when your teeth are misaligned.
+        This can lead to oral health complications if left untreated. 
+            - It may also be referred to as:
+            - crowded teeth
+            - crossbite
+            - overbite
+            - underbite
+            - open bite
+        Your teeth may not perform vital functions well, like chewing, if they’re misaligned. Learn more about this condition and how it may be treated to protect your overall oral and digestive health.
+        `,
+        img: Malocclusion
+    },
+    {
+        title: 'Tooth agenesis',
+        description: "is a congenital condition wherein a patient's mouth just didn't get directions to make some of their permanent teeth when they were born. This can range in severity from hypodontia, in which five or fewer teeth are missing, to oligodontia, when six or more permanent teeth are missing in action — this is uncommon, but often associated with genetic syndromes like Down syndrome, Van Der Woude syndrome, Reiger syndrome and ectodermal dysplasia. Anodontia is a rare recessive genetic disorder in which someone just doesn't get permanent teeth — it's also often associated with conditions like ectodermal dysplasia.",
+        img: ToothAgenesis
+    },
+    {
+        title: "Periodontal (gum) disease",
+        description: "is an infection of the tissues that hold your teeth in place. It's typically caused by poor brushing and flossing habits that allow plaque—a sticky film of bacteria—to build up on the teeth and harden. It starts with swollen, red, and bleeding gums. If left untreated, it can spread to the bones surrounding the gums, making it painful to chew. In the worst cases, teeth may become loose or need to be removed.",
+        img: Periodontal
+    },
+    {
+        title: "Tooth decay",
+        description: `
+        occurs when bacteria in the mouth produce acids that attack the enamel, or protective outer layer of the tooth. If left untreated, tooth decay could lead to gum disease, cavities, and possibly tooth loss. 
+
+        Unfortunately, tooth decay is very prevalent in the United States. According to the Centers for Disease Control and Prevention (CDC), more than 1 in 4 adults have untreated tooth decay.1 Knowing the signs and symptoms of tooth decay can lead to treatment to restore healthy teeth and gums. It’s also crucial to learn how to maintain good oral hygiene to ward off tooth decay.
+        `,
+        img: ToothDecay
+    },
+    {
+        title: "Cracked tooth syndrome (CTS)",
+        description: `
+        is where a tooth has incompletely cracked but no part of the tooth has yet broken off. Sometimes it is described as a greenstick fracture. The symptoms are very variable, making it a notoriously difficult condition to diagnose.
+
+        Cracked tooth syndrome could be considered a type of dental trauma and also one of the possible causes of dental pain. One definition of cracked tooth syndrome is "a fracture plane of unknown depth and direction passing through tooth structure that, if not already involving, may progress to communicate with the pulp and/or periodontal ligament."
+        `,
+        img: CrackedToothSyndrome
+    }
+]
---
+++ src/constants/services.ts
@@ -0,0 +1,39 @@
+import Diastema from '@/assets/images/services/services-1.png' 
+import RootCanal from '@/assets/images/services/services-2.png' 
+import DentalCrown from '@/assets/images/services/services-3.png' 
+import ToothFilling from '@/assets/images/services/services-4.png' 
+import FullDentures from '@/assets/images/services/services-5.png' 
+import Braces from '@/assets/images/services/services-6.png' 
+
+export const conditions = [
+    {
+        title: 'Tooth extraction',
+        description: 'Tooth extraction is a dental procedure that involves removing a tooth from its socket in the jawbone, often due to severe decay, damage, or overcrowding. The process begins with a thorough assessment and usually involves taking X-rays to evaluate the tooth and its surrounding structures. Local anesthesia is then administered to numb the area, ensuring the patient is comfortable and pain-free during the procedure. The dentist uses specialized tools to loosen and extract the tooth carefully. Post-procedure, patients may experience swelling or discomfort, which can be managed with prescribed medications and by following aftercare instructions provided by the dentist. Proper care and adherence to these guidelines are crucial for a smooth recovery and to avoid potential complications.',
+        img: Diastema 
+    },
+    {
+        title: 'Root canal',
+        description: 'A root canal is a dental procedure designed to treat infection or damage within the pulp of a tooth, which is the innermost soft tissue containing nerves and blood vessels. The process involves removing the infected or damaged pulp, thoroughly cleaning and disinfecting the root canals, and then filling them with a biocompatible material to seal the space. This procedure helps save the tooth and alleviate pain caused by the infection or damage. After the root canal, the tooth is typically restored with a crown or filling to ensure its strength and functionality. The procedure is generally performed under local anesthesia to ensure patient comfort.',
+        img: RootCanal 
+    },
+    {
+        title: 'Dental crown',
+        description: 'A dental crown is a cap placed over a tooth to restore its shape, size, strength, and appearance. It is typically used when a tooth is severely damaged, decayed, or after a root canal procedure. The crown covers the entire visible part of the tooth, providing protection and enhancing its function. To place a crown, the dentist first prepares the tooth by removing any damaged or decayed parts and shaping it to fit the crown. An impression of the tooth is then taken to create a custom-fitted crown, which is usually made from materials like porcelain, metal, or a combination of both. Once the crown is ready, it is cemented onto the prepared tooth, restoring its original function and appearance.',
+        img: DentalCrown 
+    },
+    {
+        title: 'Tooth filling',
+        description: "A tooth filling is a dental treatment used to repair a cavity or a small area of decay in a tooth. The process involves removing the decayed portion of the tooth and then filling the empty space with a material that restores the tooth's structure and function. The filling material, which can be made from materials such as amalgam, composite resin, glass ionomer, or gold, is applied in layers and shaped to match the tooth's natural contours. Once the filling material is in place, it is hardened and polished to ensure a smooth finish. Tooth fillings help prevent further decay and restore the tooth's ability to chew and function normally.",
+        img: ToothFilling 
+    },
+    {
+        title: 'Full dentures',
+        description: "Full dentures replace all missing teeth in either the upper or lower jaw, featuring a complete set of artificial teeth set in a pink acrylic base that mimics the gums. They are removable for cleaning and sleeping. Partial dentures, on the other hand, are used when some natural teeth remain, consisting of artificial teeth attached to a metal or acrylic framework that clasps onto the remaining teeth. Both types of dentures restore oral function, improve speech and appearance, and are custom-made for a comfortable fit.",
+        img: FullDentures 
+    },
+    {
+        title: 'Braces',
+        description: "Braces are orthodontic devices used to correct misaligned teeth and jaws, improving both dental function and appearance. They consist of metal or ceramic brackets attached to the teeth, connected by wires that apply gentle pressure to gradually shift the teeth into their proper positions. Braces can also include rubber bands or other accessories to assist with alignment. The treatment typically involves regular adjustments by an orthodontist to ensure the teeth move as planned. Braces help address issues such as overcrowding, gaps, and bite problems, leading to a more balanced and attractive smile.",
+        img: Braces 
+    },
+]

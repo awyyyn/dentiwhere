2f35aa87374c066d3ce395f49842bef19c249c41
awyn <alwin.puche16@gmail.com>
2024-09-11 08:54:39 +0800

    add actions

--- src/actions/category.ts
+++ src/actions/category.ts
@@ -1,21 +1,28 @@
  
 import { Category, DBCategory } from "@/types/types";
-import { db } from "@/utils/supabase"
-import { PostgrestSingleResponse } from "@supabase/supabase-js";
+import { db } from "@/utils/supabase";
 
 
-export const transformCategory = (data: PostgrestSingleResponse<DBCategory>): Category => {
-
-    if(data === null || typeof data === "undefined" || data.data === null) throw new Error("");
-
-    const category = data.data
-    
+export const transformCategory = (category: DBCategory): Category => {
+ 
     return {
         id: category.id,
         name: category.name,
+        clinicId: category.clinic_id,
         createdAt: category.created_at,
-        updatedAt: category.update_at!, 
+        updatedAt: category.updated_at, 
     }
+}
+
+export const getAllByClinicId = async (clinicId: number): Promise<Category[]> => {
+
+    const { data, error } = await db.from("category").select("*").eq("clinic_id", clinicId);
+
+    if(error) throw new Error(error.message)
+
+    if(data && data.length === 0) return []
+
+    return data.map(category => transformCategory(category))
 }
 
 export const getAll = async (): Promise<Category[]> => {
@@ -24,11 +31,34 @@ export const getAll = async (): Promise<Category[]> => {
 
     if(error) throw new Error(error.message)
 
-    return data.map(category => ({
-        id: category.id,
-        name: category.name,
-        createdAt: category.created_at,
-        updatedAt: category.update_at!, 
-    }))
+    if(data && data.length === 0) return []
 
+    return data.map(category => transformCategory(category))
+         
+}
+
+
+export const create = async (inputs: Omit<Category, "createdAt" | "updatedAt" | "id">): Promise<Category> => {
+
+    const { data, error } = await db.from("category").insert({
+        clinic_id: inputs.clinicId,
+        name: inputs.name,
+    }).select().maybeSingle()
+
+    if(error) throw new Error(error.message)
+
+    if(data === null) throw new Error("Error on creating category")
+        
+    return transformCategory(data)
+}
+
+export const update = async (inputs: Omit<Category, "createdAt" | "updatedAt">): Promise<Category> => {
+    const { data, error } = await db.from("category").update({
+        clinic_id: inputs.clinicId,
+        name: inputs.name,
+    }).eq("id", inputs.id).select().single();
+
+    if(error) throw new Error("Error updating category");
+
+    return transformCategory(data)   
 }

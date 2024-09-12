5345fb6cd61a8306dff381df913fb7f02ccb6bfb
awyn <alwin.puche16@gmail.com>
2024-09-12 12:49:15 +0800

    update atom

--- src/atoms/clinic-atom.ts
+++ src/atoms/clinic-atom.ts
@@ -5,7 +5,7 @@ import { db } from "@/utils/supabase";
 import { Clinic } from "@/types/types"; 
 
 
-export const clinicEditDataAtom = atom<Clinic>()
+export const clinicEditDataAtom = atom<Clinic>();
 
 export const asyncClinicAtom = atom(async (get): Promise<Clinic | null> => {
 
@@ -15,12 +15,17 @@ export const asyncClinicAtom = atom(async (get): Promise<Clinic | null> => {
   
     const response = await db
         .from("clinics")
-        .select(`*, services ( img, name, rate, description, active, category_id, clinic_id, created_at, updated_at, id )`)
+        .select(`*, 
+            services ( img, name, rate, description, active, category_id, clinic_id, created_at, updated_at, id ), 
+            amenities (id, clinic_id, name, created_at, updated_at), 
+            accessibility (id, clinic_id, name, created_at, updated_at)
+        `)
         .eq("doctor_id", typeof user.id === "number" ? user.id : 0)
          
     
     if(response.error) throw new Error(response.error.message)
- 
+   
+
     return { 
         website: response.data[0]?.website || "",
         address: response.data[0]?.address || "",
@@ -46,8 +51,27 @@ export const asyncClinicAtom = atom(async (get): Promise<Clinic | null> => {
             updatedAt: service.updated_at!,
             id: service.id,
             active: service.active
-        }))
+        })),
+        description: response.data[0]?.description ?? "", 
+        amenities: response.data[0]?.amenities?.map(ame => ({
+            clinicId: ame.clinic_id,
+            name: ame.name,
+            id: ame.id,
+            createdAt: ame.created_at,
+            updatedAt: ame.updated_at
+        })),
+        accesibilities: response.data[0]?.accessibility?.map(acc => ({
+            clinicId: acc.clinic_id,
+            name: acc.name,
+            id: acc.id, 
+            createdAt: acc.created_at,
+            updatedAt: acc.updated_at
+        })),
     } 
 })
 
-export const loadableClinicAtom = loadable(asyncClinicAtom)
+export const loadableClinicAtom = loadable(asyncClinicAtom);
+
+
+
+

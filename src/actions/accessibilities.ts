55a0595231a9f63cd68527f345f62ea1c59ee3e8
awyn <alwin.puche16@gmail.com>
2024-09-13 01:18:03 +0800

    refactor WIP

--- src/actions/accessibilities.ts
+++ src/actions/accessibilities.ts
@@ -1,32 +1,60 @@
-import { Accessibility, DBAccessibility } from '@/types/types';
-import { db } from "@/utils/supabase"  
+import { Accessibility, DBAccessibility } from "@/types/types";
+import { db } from "@/utils/supabase";
 
+const transformAccessibility = (
+	accessibility: DBAccessibility
+): Accessibility => {
+	return {
+		id: accessibility.id,
+		name: accessibility.name,
+		clinicId: accessibility.clinic_id,
+		createdAt: accessibility.created_at,
+		updatedAt: accessibility.updated_at,
+	};
+};
 
-const transformAccessibility = (accessibility: DBAccessibility): Accessibility => {
-    return {
-        id: accessibility.id,
-        name: accessibility.name,
-        clinicId: accessibility.clinic_id,
-        createdAt: accessibility.created_at,
-        updatedAt: accessibility.updated_at
-    }
-}
+export const createMany = async (
+	inputs: { clinic_id: number; name: string }[]
+): Promise<Accessibility[]> => {
+	const { data, error } = await db
+		.from("accessibility")
+		.insert(inputs)
+		.select();
+	if (error || (data && data.length === 0))
+		throw new Error("Failed to create accessibilities");
+	return data.map((accessibility) => transformAccessibility(accessibility));
+};
 
-export const createMany = async (inputs: {clinic_id: number, name: string}[]): Promise<Accessibility[]> => {
-    const { data, error } = await db.from("accessibility").insert(inputs).select(); 
-    if(error || (data && data.length === 0)) throw new Error("Failed to create accessibilities"); 
-    return data.map(accessibility => transformAccessibility(accessibility))
-}
+export const create = async (inputs: {
+	clinic_id: number;
+	name: string;
+}): Promise<Accessibility> => {
+	const { data, error } = await db
+		.from("accessibility")
+		.insert(inputs)
+		.maybeSingle();
 
+	if (error || data === null) {
+		throw new Error("Failed to create Accessibility");
+	}
 
-export const update = async (inputs: {clinic_id: number, name: string, id: string}) => {
+	return transformAccessibility(data);
+};
 
-    const { id, ...toUpdate } = inputs
+export const update = async (inputs: {
+	clinic_id: number;
+	name: string;
+	id: number;
+}) => {
+	const { id, ...toUpdate } = inputs;
 
-    const { data, error } = await db.from("accessibility").upsert(toUpdate).eq('id', id).maybeSingle();
+	const { data, error } = await db
+		.from("accessibility")
+		.update(toUpdate)
+		.eq("id", id)
+		.maybeSingle();
 
-    if(error || data === null) throw new Error("Failed to update Accessibility"); 
+	if (error || data === null) throw new Error("Failed to update Accessibility");
 
-    return transformAccessibility(data)
-
-}
+	return transformAccessibility(data);
+};
--- src/actions/amenities.ts
+++ src/actions/amenities.ts
@@ -1,38 +1,59 @@
-import { Amenities, DBAmenities } from '@/types/types';
-import { db } from "@/utils/supabase"  
-
+import { Amenities, DBAmenities } from "@/types/types";
+import { db } from "@/utils/supabase";
 
 const transformAmenities = (amenity: DBAmenities): Amenities => {
-    return {
-        id: amenity.id,
-        name: amenity.name,
-        clinicId: amenity.clinic_id,
-        createdAt: amenity.created_at,
-        updatedAt: amenity.updated_at
-    }
-}
+	return {
+		id: amenity.id,
+		name: amenity.name,
+		clinicId: amenity.clinic_id,
+		createdAt: amenity.created_at,
+		updatedAt: amenity.updated_at,
+	};
+};
 
-export const createMany = async (inputs: {clinic_id: number, name: string}[]): Promise<Amenities[]> => {
-    const { data, error } = await db.from("amenities").insert(inputs).select();
+export const createMany = async (
+	inputs: { clinic_id: number; name: string }[]
+): Promise<Amenities[]> => {
+	const { data, error } = await db.from("amenities").insert(inputs).select();
 
+	if (error || (data && data.length === 0))
+		throw new Error("Failed to create amenities");
 
-    if(error || (data && data.length === 0)) throw new Error("Failed to create amenities");
+	return data.map((amenity) => transformAmenities(amenity));
+};
 
-     
-    return data.map(amenity => transformAmenities(amenity))
-}
+export const create = async (inputs: {
+	clinic_id: number;
+	name: string;
+}): Promise<Amenities> => {
+	const { data, error } = await db
+		.from("amenities")
+		.insert(inputs)
+		.select()
+		.maybeSingle();
 
+	if (error || data === null) throw new Error("Failed to create amenities");
 
+	return transformAmenities(data);
+};
 
+export const update = async (inputs: {
+	clinic_id: number;
+	name: string;
+	id: number;
+}) => {
+	const { id, ...toUpdate } = inputs;
 
-export const update = async (inputs: {clinic_id: number, name: string, id: string}) => {
+	const { data, error } = await db
+		.from("amenities")
+		.upsert(toUpdate)
+		.eq("id", id)
+		.select()
+		.maybeSingle();
 
-    const { id, ...toUpdate } = inputs
+	console.log(error?.message, data);
 
-    const { data, error } = await db.from("amenities").upsert(toUpdate).eq('id', id).maybeSingle();
+	if (error || data === null) throw new Error("Failed to update Amenity");
 
-    if(error || data === null) throw new Error("Failed to update Amenity"); 
-
-    return transformAmenities(data)
-
-}
+	return transformAmenities(data);
+};

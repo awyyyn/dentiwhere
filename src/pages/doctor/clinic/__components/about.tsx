ba79245d918668036ced9310fe5162e4e6b21d9a
awyn <alwin.puche16@gmail.com>
2024-09-14 22:14:15 +0800

    add amenities and accessibilities

--- src/pages/doctor/clinic/__components/about.tsx
+++ src/pages/doctor/clinic/__components/about.tsx
@@ -1,13 +1,78 @@
+import { Button } from "@/components/ui/button";
+import AmenityDialog from "./amenities-dialog";
+import AccessibilityDialog from "./accesibility-dialog";
+import {
+	accessbilityDialogAtom,
+	amenitiesDialogAtom,
+} from "@/atoms/dialogs-atom";
+import { useAtomValue, useSetAtom } from "jotai";
+import { amenitiesAtom, amenityDataAtom } from "@/atoms/amenity-atom";
+import {
+	accessibilitiesAtom,
+	accessibilitiesDataAtom,
+} from "@/atoms/accessibility-atom";
+import { Separator } from "@/components/ui/separator";
+
 export default function About() {
+	const setAmenityData = useSetAtom(amenityDataAtom);
+	const setAccissibilityData = useSetAtom(accessibilitiesDataAtom);
+	const accessibilites = useAtomValue(accessibilitiesAtom);
+	const amenities = useAtomValue(amenitiesAtom);
+	const setAmenityDialog = useSetAtom(amenitiesDialogAtom);
+	const setAccessibilityDialog = useSetAtom(accessbilityDialogAtom);
+
 	return (
-		<div>
-			<h1 className="md:text-2xl font-bold tracking-wider">Description</h1>
-			<p className="md:text-xl leading-loose">
-				Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem
-				quisquam qui quaerat at, non, incidunt doloremque sunt consequatur quis
-				rerum voluptas sint aliquid saepe impedit, odio nihil cupiditate iste
-				dolorum?
-			</p>
-		</div>
+		<section className="space-y-4 mt-4">
+			<div className="flex justify-end gap-2 flex-wrap">
+				<Button
+					className="w-full sm:w-fit"
+					onClick={() => setAmenityDialog({ open: true, mode: "create" })}>
+					Add Amenity
+				</Button>
+				<Button
+					className="w-full sm:w-fit"
+					onClick={() =>
+						setAccessibilityDialog({ open: true, mode: "create" })
+					}>
+					Add Accessibility
+				</Button>
+			</div>
+			<div>
+				<h1 className="md:text-2xl font-bold tracking-wider">Description</h1>
+				<p className="md:text-xl leading-loose first-letter:ml-[2ch]">
+					Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+					Exercitationem quisquam qui quaerat at, non, incidunt doloremque sunt
+					consequatur quis rerum voluptas sint aliquid saepe impedit, odio nihil
+					cupiditate iste dolorum?
+				</p>
+			</div>
+			<Separator className="w-full h-1" />
+			<div className="grid grid-cols-2 grid-flow-row">
+				<div className="space-y-2">
+					<h1 className="md:text-2xl font-bold tracking-wider">Amenities</h1>
+					<div className="space-y-1 px-2">
+						{amenities.map((amenity) => (
+							<div className="" key={`amenity-${amenity.id}`}>
+								<p className="text-lg md:text-xl">{amenity.name}</p>
+							</div>
+						))}
+					</div>
+				</div>
+				<div className="space-y-2">
+					<h1 className="md:text-2xl font-bold tracking-wider">
+						Accessibility
+					</h1>
+					<div className="space-y-1 px-2">
+						{accessibilites.map((accessibility) => (
+							<div className="" key={`accessibility-${accessibility.id}`}>
+								<p className="text-lg md:text-xl">{accessibility.name}</p>
+							</div>
+						))}
+					</div>
+				</div>
+			</div>
+			<AmenityDialog />
+			<AccessibilityDialog />
+		</section>
 	);
 }

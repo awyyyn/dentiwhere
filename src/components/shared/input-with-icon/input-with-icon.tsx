e9018c458f3892c90c74051ad2cceec55921ce6f
awyn <alwin.puche16@gmail.com>
2024-09-15 21:58:59 +0800

    add/refactor components

--- src/components/shared/input-with-icon/input-with-icon.tsx
+++ src/components/shared/input-with-icon/input-with-icon.tsx
@@ -1,21 +1,33 @@
-import { Input, InputProps } from '@/components/ui/input' 
-import { ComponentProps, ReactElement } from 'react'  
-
+import { Input, InputProps } from "@/components/ui/input";
+import { ComponentProps, ReactElement } from "react";
 
 type Props = {
-    className: string
-    containerProps?: ComponentProps<"div">
-    inputProps?: InputProps,
-    startIcon?: ReactElement,
-    endIcon?: ReactElement 
-}
+	className: string;
+	containerProps?: ComponentProps<"div">;
+	inputProps?: InputProps;
+	startIcon?: ReactElement;
+	endIcon?: ReactElement;
+};
 
-export default function InputWithIcon({className, containerProps, startIcon, endIcon, inputProps }: Props) {
-  return (
-    <div {...containerProps} className={`flex items-center border-5 relative border-none shadow-md ${startIcon && 'pl-5 space-x-1'} ${endIcon && 'pr-5 space-x-1'} py-2 ${className}`}>
-        {startIcon && startIcon}
-        <Input {...inputProps} className={`placeholder:text-gray-500  border-none text-xl shadow-none bg-none `} />
-        {endIcon && endIcon}
-    </div>
-  )
+export default function InputWithIcon({
+	className,
+	containerProps,
+	startIcon,
+	endIcon,
+	inputProps,
+}: Props) {
+	return (
+		<div
+			{...containerProps}
+			className={`flex items-center  relative border-none shadow-md ${
+				startIcon && "pl-5 space-x-1"
+			} ${endIcon && "pr-5 space-x-1"} py-2 ${className}`}>
+			{startIcon && startIcon}
+			<Input
+				{...inputProps}
+				className={`placeholder:text-gray-500 focus-visible:ring-0 border-none text-xl shadow-none bg-none `}
+			/>
+			{endIcon && endIcon}
+		</div>
+	);
 }

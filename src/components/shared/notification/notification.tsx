f97739b910a0f489f9b75b4f7ef475fc10e36e44
awyn <alwin.puche16@gmail.com>
2024-09-18 10:27:11 +0800

    Refactor navbar component to remove unnecessary code and improve responsiveness

--- src/components/shared/notification/notification.tsx
+++ src/components/shared/notification/notification.tsx
@@ -12,22 +12,28 @@ export default function Notification() {
 		<Popover>
 			<PopoverTrigger asChild>
 				<Button
-					className="relative rounded-full group px-3 hidden sm:flex"
+					className="relative rounded-full group px-3  "
 					variant="ghost"
 					size="icon">
-					<RiNotificationFill className="scale-150 group-hover:scale-[1.8] group-hover:-rotaaste-45 transition-all" />
-
+					<RiNotificationFill
+						className="scale-150 group-hover:scale-[1.8]  
+					group-active:scale-90 transition-all"
+					/>
 					<span className="absolute text-xs   transition-all top-1 -right-1 bg-red-500 text-white rounded-full px-1 ">
 						2
 					</span>
 				</Button>
 			</PopoverTrigger>
 			<PopoverContent
-				className="w-80  hidden sm:flex  
-                    data-[state=open]:-translate-x-20 
-                    data-[state=closed]:-translate-x-20     
-                    md:data-[state=open]:-translate-x-36 
-                    md:data-[state=closed]:-translate-x-36 
+				className="
+					w-72 
+                    data-[state=open]:-translate-x-5 
+                    data-[state=closed]:-translate-x-5  
+					md:w-80    
+                    md:data-[state=open]:-translate-x-24 
+                    md:data-[state=closed]:-translate-x-24 
+                    lg:data-[state=open]:-translate-x-16 
+                    lg:data-[state=closed]:-translate-x-16 
                 "
 				arrowPadding={2}
 				side="bottom">

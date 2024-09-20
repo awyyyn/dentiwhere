870a5a07d2191735e8cc87561212cfc31b3ee294
awyn <alwin.puche16@gmail.com>
2024-09-20 22:56:48 +0800

    add doctor view details page and doctorAtom

--- src/atoms/doctors-atom.ts
+++ src/atoms/doctors-atom.ts
@@ -2,3 +2,5 @@ import { User } from "@/types/types";
 import { atom } from "jotai";
 
 export const doctorsAtom = atom<User[]>([]);
+
+export const doctorAtom = atom<User & { clinicName: string }>();

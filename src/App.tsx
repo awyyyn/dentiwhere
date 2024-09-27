8041fc30cbc0cd8519e2d7296924bf17a0d87cc5
awyn <alwin.puche16@gmail.com>
2024-09-27 01:12:34 -1200

    Refactor routing in App.tsx and add public view clinics page
    Refactored the routing in App.tsx to include a new route for the public view clinics page. Imported the PublicViewClinics component and added a corresponding route in the routes array. This allows users to view all clinics without authentication.
    
    Also, added a new atom and loadable for clinics data
    
    Added a new atom, clinicsLoadableAtom, and a corresponding loadable function to fetch all clinics data using the getAllClinics action. This will be used to asynchronously load the clinics data and update the clinicsAtom state.

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,8 @@ import Doctors from "./pages/admin/doctors/doctors";
 import Doctor from "./pages/admin/doctor/doctor";
 import Clinics from "./pages/admin/clinics/clinics";
 import Clinic from "./pages/public/clinic/clinic";
-import {VerifyDoctor} from "@/pages/admin/verify-doctor/verify-doctor.tsx";
+import PublicViewClinics from "./pages/public/clinics/clinics";
+import { VerifyDoctor } from "@/pages/admin/verify-doctor/verify-doctor.tsx";
 
 const Login = Loadable(lazy(() => import("./pages/auth/login/login")));
 const SignUp = Loadable(lazy(() => import("./pages/auth/sign-up/sign-up")));
@@ -77,6 +78,10 @@ export default function App() {
 							path: "clinics/view/:id",
 							element: <Clinic />,
 						},
+						{
+							path: "clinics",
+							element: <PublicViewClinics />,
+						},
 					],
 				},
 				{
@@ -97,7 +102,7 @@ export default function App() {
 						},
 						{
 							path: "doctors/verify/:id",
-							element: <VerifyDoctor />
+							element: <VerifyDoctor />,
 						},
 						{
 							path: "clinics",
--- src/atoms/clinic-atom.ts
+++ src/atoms/clinic-atom.ts
@@ -1,5 +1,7 @@
 import { atom } from "jotai";
 import { Clinic, ClinicWithDoctor } from "@/types/types";
+import { loadable } from "jotai/utils";
+import { getAllClinics } from "@/actions";
 
 export const clinicEditDataAtom = atom<Clinic>();
 
@@ -8,3 +10,9 @@ export const clinicAtom = atom<Clinic>();
 export const clinicWithDoctorAtom = atom<ClinicWithDoctor>();
 
 export const clinicsAtom = atom<ClinicWithDoctor[]>([]);
+
+const clinicsLoadableAtom = atom(async () => {
+	return await getAllClinics();
+});
+
+export const clinicsLoadable = loadable(clinicsLoadableAtom);

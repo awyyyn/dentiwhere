85e6d22cc2df6efa9fb44f0fc637baf8ecb9a331
awyn <alwin.puche16@gmail.com>
2024-09-27 21:29:14 -0400

    add sample carouse WIP

--- src/pages/public/home/home.tsx
+++ src/pages/public/home/home.tsx
@@ -4,11 +4,13 @@ import Hero from "./__components/hero/hero";
 import ListOfClinics from "./__components/list-of-clinics/list-of-clinics";
 import QrLink from "./__components/qr-link/qr-link";
 import Services from "./__components/services/services";
+import PlacesCarousel from "./__components/carousel/carousel";
 
 export default function Home() {
 	return (
 		<>
 			<Hero />
+			<PlacesCarousel />
 			<QrLink />
 			<ListOfClinics />
 			<Conditions />

cf4cfb7f5094f8f526c5ce43777a1a5760e20c49
awyn <alwin.puche16@gmail.com>
2024-10-02 08:20:51 +0800

    WIP

--- src/components/shared/footer/footer.tsx
+++ src/components/shared/footer/footer.tsx
@@ -1,80 +1,100 @@
-import { Link } from 'react-router-dom'
-import { FaSquareXTwitter, FaSquareFacebook, FaSquareInstagram } from "react-icons/fa6";
-import { Button } from '@/components/ui/button';
+import { Link } from "react-router-dom";
+import {
+	FaSquareXTwitter,
+	FaSquareFacebook,
+	FaSquareInstagram,
+} from "react-icons/fa6";
+import { Button } from "@/components/ui/button";
 
 export default function Footer() {
+	const patientLinks = [
+		{ path: "/", name: "" },
+		{ path: "conditions", name: "Conditions" },
+		{ path: "services", name: "Services" },
+		{ path: "book-now", name: "Book Now" },
+		{ path: "inquire-now", name: "Inquire Now" },
+	];
 
+	const generalLinks = [
+		{ path: "about-us", name: "About Us" },
+		{ path: "terms-and-conditions", name: "Terms and Conditions" },
+		{ path: "", name: "Privacy Policy" },
+	];
 
-  const patientLinks = [
-    {path: "/", name: ""},
-    {path: "conditions", name: "Conditions"},
-    {path: "services", name: "Services"},
-    {path: "book-now", name: "Book Now"},
-    {path: "inquire-now", name: "Inquire Now"},
-    {path: "sign-up", name: "Create an Account"}, 
-    {path: "login", name: "Log in"},
-  ]
+	const doctorsLinks = [
+		{ path: "list-your-dental-clinic", name: "List Your Dental Clinic" },
+		{ path: "sign-up", name: "Create an Account" },
+		{ path: "login", name: "Log in" },
+	];
 
-  const generalLinks = [
-    {path: "about-us", name: "About Us"},
-    {path: "terms-and-conditions", name: "Terms and Conditions"},
-    {path: "", name: "Privacy Policy"},
-  ]
-
-  const doctorsLinks = [
-    {path: "list-your-dental-clinic", name: "List Your Dental Clinic"},
-  ]
-
-  return (
-    <footer className='bg-[#D9D9D9] py-20 shadow-2xl mb-10'>
-      <div className="w-11/12 mx-auto md:w-10/12 gap-y-5 sm:gap-10 grid sm:grid-cols-3   "> 
-        <div className='flex flex-col gap-y-5 sm:gap-y-none justify-between h-full   '> 
-          <div className='space-y-3'> 
-            <h1 className='text-lg font-bold sm:text-2xl'>General</h1>
-            <div className='space-y-3  pl-5'>
-              {generalLinks.map(({name, path}, indx) => (
-                <Link to={path} key={`general-link-${path}-${indx}`} className='block hover:drop-shadow-lg transition-all '>
-                  {name}
-                </Link> 
-              ))}
-            </div>
-          </div>
-          <div className='space-y-3'>
-              <h1 className='text-lg font-bold sm:text-2xl'>Social Media</h1>
-              <div className='flex flex-row gap-x-5   pl-5'>
-                <Button size="icon" variant="ghost" className='hover:shadow-xl transition-shadow duration-300'>
-                  <FaSquareXTwitter size={40} />
-                </Button>
-                <Button size="icon" variant="ghost" className='hover:shadow-xl transition-shadow duration-300'>
-                  <FaSquareFacebook size={40} /> 
-                </Button>
-                <Button size="icon" variant="ghost" className='hover:shadow-xl transition-shadow duration-300'>
-                  <FaSquareInstagram size={40} />
-                </Button>
-              </div>
-          </div>
-        </div>
-        <div className='space-y-3 flex flex-col sm:items-center'>
-          <h1 className='text-lg font-bold sm:text-2xl'>For Patients</h1>
-          <div className='space-y-3 sm:pl-10 pl-5'>
-            {patientLinks.map(({name, path}, indx) => (
-              <Link to={path} key={`patient-link-${path}-${indx}`} className='block'>
-                {name}
-              </Link> 
-            ))}
-          </div>
-        </div>
-        <div className='space-y-3  flex flex-col sm:items-center'>
-          <h1 className='text-lg font-bold sm:text-2xl'>For Doctors</h1>
-          <div className='space-y-3 sm:pl-10 pl-5' >
-            {doctorsLinks.map(({name, path}, indx) => (
-              <Link to={path} key={`doctor-link-${path}-${indx}`} className='block'>
-                {name}
-              </Link> 
-            ))}
-          </div>
-        </div>
-      </div>
-    </footer>
-  )
+	return (
+		<footer className="bg-[#D9D9D9] py-20 shadow-2xl mb-10">
+			<div className="w-11/12 mx-auto md:w-10/12 gap-y-5 sm:gap-10 grid sm:grid-cols-3   ">
+				<div className="flex flex-col gap-y-5 sm:gap-y-none justify-between h-full   ">
+					<div className="space-y-3">
+						<h1 className="text-lg font-bold sm:text-2xl">General</h1>
+						<div className="space-y-3  pl-5">
+							{generalLinks.map(({ name, path }, indx) => (
+								<Link
+									to={path}
+									key={`general-link-${path}-${indx}`}
+									className="block hover:drop-shadow-lg transition-all ">
+									{name}
+								</Link>
+							))}
+						</div>
+					</div>
+					<div className="space-y-3">
+						<h1 className="text-lg font-bold sm:text-2xl">Social Media</h1>
+						<div className="flex flex-row gap-x-5   pl-5">
+							<Button
+								size="icon"
+								variant="ghost"
+								className="hover:shadow-xl transition-shadow duration-300">
+								<FaSquareXTwitter size={40} />
+							</Button>
+							<Button
+								size="icon"
+								variant="ghost"
+								className="hover:shadow-xl transition-shadow duration-300">
+								<FaSquareFacebook size={40} />
+							</Button>
+							<Button
+								size="icon"
+								variant="ghost"
+								className="hover:shadow-xl transition-shadow duration-300">
+								<FaSquareInstagram size={40} />
+							</Button>
+						</div>
+					</div>
+				</div>
+				<div className="space-y-3 flex flex-col sm:items-center">
+					<h1 className="text-lg font-bold sm:text-2xl">For Patients</h1>
+					<div className="space-y-3 sm:pl-10 pl-5">
+						{patientLinks.map(({ name, path }, indx) => (
+							<Link
+								to={path}
+								key={`patient-link-${path}-${indx}`}
+								className="block">
+								{name}
+							</Link>
+						))}
+					</div>
+				</div>
+				<div className="space-y-3  flex flex-col sm:items-center">
+					<h1 className="text-lg font-bold sm:text-2xl">For Doctors</h1>
+					<div className="space-y-3 sm:pl-10 pl-5">
+						{doctorsLinks.map(({ name, path }, indx) => (
+							<Link
+								to={path}
+								key={`doctor-link-${path}-${indx}`}
+								className="block">
+								{name}
+							</Link>
+						))}
+					</div>
+				</div>
+			</div>
+		</footer>
+	);
 }
--- src/pages/public/home/__components/carousel/carousel.tsx
+++ src/pages/public/home/__components/carousel/carousel.tsx
@@ -1,9 +1,16 @@
 import { Card, CardContent } from "@/components/ui/card";
+import { AsyncImage } from "loadable-image";
 import {
 	Carousel,
 	CarouselContent,
 	CarouselItem,
 } from "@/components/ui/carousel";
+
+// GUINOBATAN = https://scontent.flgp1-1.fna.fbcdn.net/v/t1.6435-9/57297836_2343997612536105_9196792262376292352_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=2a1932&_nc_eui2=AeE0LRx_j-rscT5OMxnGQaNChKExZWNvWfSEoTFlY29Z9GQSfD0tiQJzSuFfNY8VD4CHlgY55bfz-8ccUOFxN-KN&_nc_ohc=nYdDfjEMiqsQ7kNvgHd1Ojk&_nc_ht=scontent.flgp1-1.fna&_nc_gid=ArwxxXJvNENlCTtrpFm_fNJ&oh=00_AYAWXK7F8qWXs-ZgqJxqssLmEaIc-K5fu36vnO4XClHowA&oe=67222B14
+
+// LIGAO = https://scontent.flgp1-1.fna.fbcdn.net/v/t39.30808-6/460843895_3822435394751043_2073815357668793575_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=86c6b0&_nc_eui2=AeF9mH835tSaDtoxWOmtnVuMfCBAAxISxq18IEADEhLGrfEdTugEoMMckXJLpMahpykWJ81BVk_XfXMipoFUYFC7&_nc_ohc=SJ7M8g0xfjgQ7kNvgHl_sRm&_nc_ht=scontent.flgp1-1.fna&_nc_gid=AUDxKKAjSLUGYVqx7TFzW-b&oh=00_AYDOFynhyUB7oNR7yRrIhBPyESRnVpg1NEpkFEJ6V3YDDw&oe=670096F2
+
+//
 
 export default function CarouselSize() {
 	return (
@@ -17,14 +24,13 @@ export default function CarouselSize() {
 					<CarouselContent>
 						{Array.from({ length: 5 }).map((_, index) => (
 							<CarouselItem key={index} className="basis-96">
-								<div className="p-1">
-									<Card>
-										<CardContent className="flex aspect-square items-center justify-center p-6">
-											<span className="text-3xl font-semibold">
-												{index + 1}
-											</span>
-										</CardContent>
-									</Card>
+								<div className="p-1 h-[300px]">
+									<AsyncImage
+										className="h-full w-full object-cover"
+										src="https://scontent.flgp1-1.fna.fbcdn.net/v/t39.30808-6/460843895_3822435394751043_2073815357668793575_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=86c6b0&_nc_eui2=AeF9mH835tSaDtoxWOmtnVuMfCBAAxISxq18IEADEhLGrfEdTugEoMMckXJLpMahpykWJ81BVk_XfXMipoFUYFC7&_nc_ohc=SJ7M8g0xfjgQ7kNvgHl_sRm&_nc_ht=scontent.flgp1-1.fna&_nc_gid=AUDxKKAjSLUGYVqx7TFzW-b&oh=00_AYDOFynhyUB7oNR7yRrIhBPyESRnVpg1NEpkFEJ6V3YDDw&oe=670096F2"
+										srcSet="https://scontent.flgp1-1.fna.fbcdn.net/v/t39.30808-6/460843895_3822435394751043_2073815357668793575_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=86c6b0&_nc_eui2=AeF9mH835tSaDtoxWOmtnVuMfCBAAxISxq18IEADEhLGrfEdTugEoMMckXJLpMahpykWJ81BVk_XfXMipoFUYFC7&_nc_ohc=SJ7M8g0xfjgQ7kNvgHl_sRm&_nc_ht=scontent.flgp1-1.fna&_nc_gid=AUDxKKAjSLUGYVqx7TFzW-b&oh=00_AYDOFynhyUB7oNR7yRrIhBPyESRnVpg1NEpkFEJ6V3YDDw&oe=670096F2"
+										alt="ligao"
+									/>
 								</div>
 							</CarouselItem>
 						))}

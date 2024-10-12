660f7aff746321791a0fa19b4011cb0b6f3206e3
awyn <alwin.puche16@gmail.com>
2024-10-12 22:18:56 +0800

    Refactor package.json to add @radix-ui/react-alert-dialog and react-responsive-carousel dependencies

--- src/pages/public/home/__components/carousel/carousel.tsx
+++ src/pages/public/home/__components/carousel/carousel.tsx
@@ -1,4 +1,6 @@
 import { AsyncImage } from "loadable-image";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel as CarouselAuto } from "react-responsive-carousel";
 import {
 	Carousel,
 	CarouselContent,
@@ -13,6 +15,14 @@ import Ligao from "@/assets/images/third_district/ligao.png";
 import Oas from "@/assets/images/third_district/oas.png";
 import Pioduran from "@/assets/images/third_district/pioduran.png";
 import Polangui from "@/assets/images/third_district/polangui.png";
+import Guinobatan2 from "@/assets/images/third_district/municipality-of-guinobatan.jpg";
+import Jovellar2 from "@/assets/images/third_district/municipality-of-jovellar.jpg";
+import Libon2 from "@/assets/images/third_district/mucipality-of-libon.jpg";
+import Ligao2 from "@/assets/images/third_district/city-of-ligao.jpg";
+// import Oas2 from "@/assets/images/third_district/oas.png";
+// import Pioduran2 from "@/assets/images/third_district/pioduran.png";
+import Polangui2 from "@/assets/images/third_district/mucipality-of-polangui.jpg";
+import { AspectRatio } from "@/components/ui/aspect-ratio";
 
 const items = [
 	{
@@ -45,9 +55,32 @@ const items = [
 	},
 ];
 
+const items2 = [
+	{
+		img: Guinobatan2,
+		name: "Guinobatan",
+	},
+	{
+		img: Jovellar2,
+		name: "Jovellar",
+	},
+	{
+		img: Libon2,
+		name: "Libon",
+	},
+	{
+		img: Ligao2,
+		name: "Ligao",
+	},
+	{
+		img: Polangui2,
+		name: "Polangui",
+	},
+];
+
 export default function CarouselSize() {
 	return (
-		<section className="  overflow-hidden py-10 md:py-0 ">
+		<section className="  overflow-hidden py-10 md:py-0 space-y-10 ">
 			<div className="flex md:flex-row flex-col justify-between   mx-auto w-11/12 md:w-10/12">
 				<Carousel
 					opts={{
@@ -74,6 +107,33 @@ export default function CarouselSize() {
 					{/* <CarouselPrevious />
 					<CarouselNext /> */}
 				</Carousel>
+			</div>
+			<div className="flex select-none md:flex-row flex-col justify-between   mx-auto w-11/12 md:w-10/12">
+				<CarouselAuto
+					showArrows={false}
+					swipeable
+					emulateTouch
+					transitionTime={700}
+					autoPlay
+					infiniteLoop
+					stopOnHover
+					interval={5000}
+					className="   w-full max-h-[700px]">
+					{items2.map((item, index) => (
+						<div
+							key={index}
+							className="relative min-h-[700px] w-full h-[700px] group  ">
+							{/* <div className="capitalize text-xl tracking-wide text-left px-5 font-bold z-50 absolute w-full  py-5 -bottom-full group-hover:bottom-0 transition-all duration-500 left-0 backdrop-blur-ssm bg-white/60">
+								{item.name}
+							</div> */}
+							<img
+								src={item.img}
+								className="absolute min-h-[700px] h-[700px] top-0 left-0   w-full "
+								alt="Carousel item"
+							/>
+						</div>
+					))}
+				</CarouselAuto>
 			</div>
 		</section>
 	);
